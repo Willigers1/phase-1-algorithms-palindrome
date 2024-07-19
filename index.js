28fe4cf618b326d1ffaf2f75339850f483d50a4d
Willigers <ronowilligers@gmail.com>
2024-07-19 10:01:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 function isPalindrome(word) {
+  for (let i = 0; i < word; i++) {}
+
   // Write your algorithm here
 }
+function reverseString(word) {
+  // TODO: implement string reversing functionality
+  return word;
+}
 
-/* 
-  Add your pseudocode here
-*/
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isPalindrome(word) {
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+//Add your pseudocode here
 
 /*
-  Add written explanation of your solution here
-*/
+    Add written explanation of your solution here
+  */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
